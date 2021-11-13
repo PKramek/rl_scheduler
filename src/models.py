31df8533369efe95a723e7af435cb606ac8c81a5
Pkramek <peterkramek@gmail.com>
2021-11-13 20:48:03 +0100

    Add endpoints for results information

--- src/models.py
+++ src/models.py
@@ -1,4 +1,46 @@
+from sqlalchemy import ForeignKey
+
 from src import db
+
+
+class Algorithm(db.Model):
+    __tablename__ = 'algorithm'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+
+    def __repr__(self):
+        return f"<Algorithm(name={self.name})>"
+
+
+class TrainingResults(db.Model):
+    __tablename__ = 'training_results'
+    result_id = db.Column(db.Integer, primary_key=True)
+    best_mean_result = db.Column(db.Float, nullable=False)
+    results_subdirectory = db.Column(db.String, nullable=False)
+    environment = db.Column(db.String, nullable=False)
+    algorithm_config = db.Column(db.String, nullable=False)
+    date = db.Column(db.TIMESTAMP(), nullable=False)
+    algorithm = db.Column(db.Integer, ForeignKey('algorithm.id'))
+
+    def __repr__(self):
+        return f"""<TrainingResults(best_mean_result={self.best_mean_result},
+                results_subdirectory={self.results_subdirectory},
+                environment={self.environment},
+                algorithm_config={self.algorithm_config},
+                date={self.date},
+                algorithm={self.algorithm}
+                )>"""
+
+    def to_dict(self):
+        return {
+            "result_id": self.result_id,
+            "best_mean_result": self.best_mean_result,
+            "results_subdirectory": self.results_subdirectory,
+            "environment": self.environment,
+            "algorithm_config": self.algorithm_config,
+            "date": self.date,
+            "algorithm": Algorithm.query.get(self.algorithm).name
+        }
 
 
 class Users(db.Model):
---
+++ src/repository.py
@@ -0,0 +1,32 @@
+from sqlalchemy import desc
+
+from src.models import Algorithm, TrainingResults
+
+
+class AlgorithmRepository:
+    @staticmethod
+    def get_algorithm_by_name(name: str) -> Algorithm:
+        return Algorithm.query.filter(Algorithm.name == name).first()
+
+    @staticmethod
+    def get_algorithm_by_id(algorithm_id: int):
+        return Algorithm.query.get(algorithm_id)
+
+
+class TrainingResultsRepository:
+    @staticmethod
+    def get_all_results():
+        return TrainingResults.query.order_by(
+            desc(TrainingResults.environment),
+            desc(TrainingResults.best_mean_result)
+        ).all()
+
+    @staticmethod
+    def get_results_for_algorithm(algorithm_id: int):
+        return TrainingResults.query.filter(TrainingResults.algorithm == algorithm_id).order_by(
+            desc(TrainingResults.environment), desc(TrainingResults.best_mean_result)).all()
+
+    @staticmethod
+    def get_results_for_environment(environment: str):
+        return TrainingResults.query.filter(TrainingResults.environment == environment).order_by(
+            desc(TrainingResults.best_mean_result)).all()
